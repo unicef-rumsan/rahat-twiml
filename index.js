1b5e1b3c9bf965ff515a4d768926be7fc258b76f
Raghav Kattel <arg.ahv7@gmail.com>
2023-01-04 10:54:14 +0545

    upload create call through twiml

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 require("dotenv").config();
-const urlencoded = require("body-parser").urlencoded;
+const bodyParser = require("body-parser");
 const cors = require("cors");
-const express = require('express')
-const fileUpload = require("express-fileupload")
-const app = express()
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
 const port = process.env.PORT || 3000;
-const indexRouter = require("./routes/index")
+const indexRouter = require("./routes/index");
 
-app.use(cors())
+app.use(cors());
 app.use(fileUpload());
 
-app.use(urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(express.static('assets'))
+app.use(express.static("assets"));
 
-app.use('/api/v1', indexRouter)
+app.use("/api/v1", indexRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from twilio markup API" })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from twilio markup API" });
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({ message: "route not found " });
-})
+});
 
 app.listen(port, () => {
-  console.log(`application listening on port: ${port}`)
-})
+  console.log(`application listening on port: ${port}`);
+});
